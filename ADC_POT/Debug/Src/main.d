9e14597b7da0c70de6f43ce9fc5fa69fd4585b56
Ahmed Tarek Youssef <126518618+AhmedTarekYoussif@users.noreply.github.com>
2023-03-22 23:41:23 +0200

    Add files via upload

---
+++ ADC_POT/Debug/Src/main.d
@@ -0,0 +1,9 @@
+Src/main.o: ../Src/main.c ../Inc/STD_Types.h ../Inc/BIT_Math.h \
+ ../Inc/RCC_interface.h ../Inc/DIO_interface.h ../Inc/ADC_int.h \
+ ../Inc/LCD_int.h
+../Inc/STD_Types.h:
+../Inc/BIT_Math.h:
+../Inc/RCC_interface.h:
+../Inc/DIO_interface.h:
+../Inc/ADC_int.h:
+../Inc/LCD_int.h:
